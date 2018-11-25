29cff7104870a98ea091ca07ad3f54619d85eb24
Surat Teerapittayanon <steerapi@gmail.com>
2018-11-25 13:34:21 -0500

    bump version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-const { promisify } = require('promise-callbacks');
-
 const w = (promise) => {
   if (promise) {
     return promise.then((data) => [null, data]).catch((error) => [error, null]);
@@ -7,8 +5,5 @@ const w = (promise) => {
     return Promise.reject(new Error('promise is undefined'));
   }
 };
-const p = (context, fnName) => {
-  return promisify.methods(context, [fnName])[fnName];
-};
 
-export { w, p };
+export { w };
